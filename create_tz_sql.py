c9023a372fd97704ca10c7efbf87e9cf3520aa04
Brent MacArthur <bmacarthur67@gmail.com>
2019-12-08 15:57:29 -0800

    Test with statement

--- create_tz_sql.py
+++ create_tz_sql.py
@@ -4,15 +4,10 @@ import os
 
 path = 'D:\\My Documents\\Education\\Cloud Computing\\CloudComputing-FinalAssignment'
 write_file = os.path.join(path, 'insert_time_zones.sql')
-read_file = os.path.join(path, 'tz.csv')
+read_file = os.path.join(path, 'tz_nh.csv')
 
 # with open(path + 'insert_time_zones.sql', 'w') as file_w:
-# with open(read_file, 'w') as file_r:
-#    for line in file_r:
-#        print(line)
-print(read_file)
-file_r = open(read_file)
-for line in file_r:
-    line = line.rstrip()
-    print(line)
-file_r.close()
+with open(read_file, 'r') as file_r:
+    for line in file_r:
+        line = line.rstrip()
+        print(line)
