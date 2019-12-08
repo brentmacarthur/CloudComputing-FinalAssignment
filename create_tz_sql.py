40cc5b95fcaaaa98dac9cb5759bdd548b2113dca
Brent MacArthur <bmacarthur67@gmail.com>
2019-12-08 15:51:17 -0800

    Test read

--- create_tz_sql.py
+++ create_tz_sql.py
@@ -1,4 +1,17 @@
 # Create SQL Insert script
 
-with open('D:\My Documents\Education\Cloud Computing\CloudComputing-FinalAssignment\insert_time_zones.sql, 'w')
-with open()
+import os
+
+path = 'D:\\My Documents\\Education\\Cloud Computing\\CloudComputing-FinalAssignment'
+write_file = os.path.join(path, 'insert_time_zones.sql')
+read_file = os.path.join(path, 'tz.csv')
+
+# with open(path + 'insert_time_zones.sql', 'w') as file_w:
+# with open(read_file, 'w') as file_r:
+#    for line in file_r:
+#        print(line)
+print(read_file)
+file_r = open(read_file)
+for line in file_r:
+    line = line.rstrip()
+    print(line)
